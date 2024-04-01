b8eb82472ab5a18f41d7c8e52138008ad147824e
Hanna Borodina <venera131916@gmail.com>
2024-04-01 12:55:43 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+const string IP_SERVER_ADR = "127.0.0.1";
+const int PORT_SERVER_ADDR = 4000;
+
+using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+await socket.ConnectAsync(IP_SERVER_ADR, PORT_SERVER_ADDR);
+
+Console.WriteLine("Client");
+Console.WriteLine("Client connected to server");
+
+socket.Send(Encoding.Unicode.GetBytes(Console.ReadLine()));
